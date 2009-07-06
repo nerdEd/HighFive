2d0b5cc5f8a49199040f0f17b0ea10ffd7745272
Ed Schmalzle <Jonas714@gmail.com>
2009-07-06 17:39:37 -0400

    updated gem configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+# Setup gem path for production
+ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
+
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
