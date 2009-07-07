6fa8f15e4b12495b156fea3bc4f2073a3df5905c
Ed Schmalzle <Jonas714@gmail.com>
2009-07-07 10:34:41 -0400

    updated shell script to properly point to the rake executable

--- update_scores.sh
+++ update_scores.sh
@@ -1,7 +1,7 @@
 ##!/usr/bin/env bash
 
 cd ~/highfive.edschmalzle.com/current
-rake highfive:score
+/usr/bin/rake highfive:score
 cp db/development.sqlite3 db/production.sqlite3
 touch tmp/restart.txt
 
