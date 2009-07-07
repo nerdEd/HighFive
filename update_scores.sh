16428ce7c619c53d6921f76929d3c37cce2d3792
Ed Schmalzle <Jonas714@gmail.com>
2009-07-07 09:30:11 -0400

    updated bash script for cron so their is no interruption of service

--- update_scores.sh
+++ update_scores.sh
@@ -1,5 +1,8 @@
 ##!/usr/bin/env bash
 
 cd ~/highfive.edschmalzle.com/current
-rake highfive:score RAILS_ENV=production
+rake highfive:score
+cp db/development.sqlite3 db/production.sqlite3
 touch tmp/restart.txt
+
+echo "Photo Update Complete"
