afae7d2c6608220119fbfc0e604efe7367a538a1
Ed Schmalzle <Jonas714@gmail.com>
2009-07-07 19:30:49 -0400

    removed environment specific path setup from photo update script

--- update_scores.sh
+++ update_scores.sh
@@ -1,10 +1,4 @@
 ##!/usr/bin/env bash
-
-export PATH=~/.gems/bin:$PATH
-
-export GEM_HOME=$HOME/.gems
-export GEM_PATH=$GEM_HOME:/usr/lib/ruby/gems/1.8
-
 cd ~/highfive.edschmalzle.com/current
 /usr/bin/rake highfive:score
 cp db/development.sqlite3 db/production.sqlite3
