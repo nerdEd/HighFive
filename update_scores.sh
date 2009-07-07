f1d18f4181b4e9c65f258369a11ff2dccd43b08d
Ed Schmalzle <Jonas714@gmail.com>
2009-07-07 11:15:31 -0400

    added path updates to photo update script

--- update_scores.sh
+++ update_scores.sh
@@ -1,8 +1,11 @@
 ##!/usr/bin/env bash
 
+export PATH=~/.gems/bin:$PATH
+
+export GEM_HOME=$HOME/.gems
+export GEM_PATH=$GEM_HOME:/usr/lib/ruby/gems/1.8
+
 cd ~/highfive.edschmalzle.com/current
 /usr/bin/rake highfive:score
 cp db/development.sqlite3 db/production.sqlite3
 touch tmp/restart.txt
-
-echo "Photo Update Complete"
