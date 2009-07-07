65bdea2e0ebcd0fd4ea53437cbfc8e3f9c9c0a68
Ed Schmalzle <Jonas714@gmail.com>
2009-07-07 01:14:20 -0400

    implemented moderation for scoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,20 @@ class User < ActiveRecord::Base
   has_many :photos
   
   def calculate_score
-    photos.size
+    total_score = photos.size
+    photos.each do | photo |
+      photo.comments.each do | comment |
+        if( comment.author_name = APP_CONFIG[ 'admin' ] )
+          mods = comment.text.scan( /\[[+-]\d*\]/ )
+          if( !mods.empty? )
+            mods.each do | mod |
+              total_score += mod.gsub( /[\[\]]/, '' ).to_i
+            end
+          end
+        end
+      end
+    end
+    total_score
   end
   
 end
