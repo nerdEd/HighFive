f38545466a85bdbd2d2b331aef79c4efd822ebc2
Ed Schmalzle <Jonas714@gmail.com>
2009-07-08 16:51:20 -0400

    removed update scores shell script

--- update_scores.sh
+++
@@ -1,6 +0,0 @@
-##!/usr/bin/env bash
-cd ~/highfive.edschmalzle.com/current
-cp ../shared/production.sqlite3 db/development.sqlite3
-/usr/bin/rake highfive:score
-cp db/development.sqlite3 ../shared/production.sqlite3
-touch tmp/restart.txt
