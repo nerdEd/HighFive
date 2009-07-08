3e4b3730ea522546620b3db3b0af2b41c61d358e
Ed Schmalzle <Jonas714@gmail.com>
2009-07-08 15:40:14 -0400

    updated the update script to work with new database location

--- update_scores.sh
+++ update_scores.sh
@@ -1,5 +1,6 @@
 ##!/usr/bin/env bash
 cd ~/highfive.edschmalzle.com/current
+cp ../shared/production.sqlite3 db/development.sqlite3
 /usr/bin/rake highfive:score
-cp db/development.sqlite3 db/production.sqlite3
+cp db/development.sqlite3 ../shared/production.sqlite3
 touch tmp/restart.txt
